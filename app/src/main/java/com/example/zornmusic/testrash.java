8b9bd1f7dc2456dda30d1f2b5d68afdae687a79c
IT21013850 <it21013850@my.sliit.lk>
2022-10-11 23:51:08 +0530

    testRash

---
+++ app/src/main/java/com/example/zornmusic/testrash.java
@@ -0,0 +1,14 @@
+package com.example.zornmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testrash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testrash);
+    }
+}
