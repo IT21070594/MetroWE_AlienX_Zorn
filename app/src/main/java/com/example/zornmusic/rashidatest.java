be26bccb583f36f80ed82c6ff3f3fd2dbd187ce5
IT21013850 <it21013850@my.sliit.lk>
2022-10-12 15:24:02 +0530

    rashida testing

---
+++ app/src/main/java/com/example/zornmusic/rashidatest.java
@@ -0,0 +1,14 @@
+package com.example.zornmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rashidatest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rashidatest);
+    }
+}
