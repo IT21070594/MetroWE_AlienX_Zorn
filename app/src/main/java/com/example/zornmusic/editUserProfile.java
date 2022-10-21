420bace28ea54ded36096b38566d4b09a46b76c5
it21004568 <it21004568@my.sliit.lk>
2022-10-21 22:53:47 +0530

    NUHA'S FILES

---
+++ app/src/main/java/com/example/zornmusic/editUserProfile.java
@@ -0,0 +1,14 @@
+package com.example.zornmusic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class editUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
---
+++ app/src/main/java/com/example/zornmusic/viewUserPlan.java
@@ -0,0 +1,14 @@
+package com.example.zornmusic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class viewUserPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user_plan);
+    }
+}
