5aa6387f1f28db5b87227122ba86d8b91f2bde46
ASUS <it21112096@my.sliit.lk>
2022-10-22 11:46:04 +0530

    ONELLA'S FILES

---
+++ app/src/main/java/com/example/zornmusic/ShowPlaylistContents.java
@@ -0,0 +1,14 @@
+package com.example.zornmusic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowPlaylistContents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_playlist_contents);
+    }
+}
