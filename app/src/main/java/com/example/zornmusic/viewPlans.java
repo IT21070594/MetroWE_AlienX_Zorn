1f8aeb77cc827b22cc4d70644da0d9f3a3afc983
IT21013850 <it21013850@my.sliit.lk>
2022-10-25 13:35:07 +0530

    Intent for upgrade

--- app/src/main/java/com/example/zornmusic/viewPlans.java
+++ app/src/main/java/com/example/zornmusic/viewPlans.java
@@ -2,6 +2,8 @@ package com.example.zornmusic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,21 @@ Intent viewPlans ;
         viewPlans = getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_plans);
+        Button home = findViewById(R.id.homeBtn);
+        Button BtnLibrary = findViewById(R.id.myLibraryBtn);
+        BtnLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent library = new Intent(getApplicationContext(), myLibrary.class);
+                startActivity(library);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentHome = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intentHome);
+            }
+        });
     }
 }
